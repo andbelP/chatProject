9ef34183b0e65e4fdf80a55e7a44f3cf0e94ab85
Andrey <96589132+andbelP@users.noreply.github.com>
2024-12-04 23:46:51 +0500

    sdsd

--- app/src/main/java/com/example/chatproject/bottomnav/newchat/NewChatFragment.java
+++ app/src/main/java/com/example/chatproject/bottomnav/newchat/NewChatFragment.java
@@ -1,5 +1,6 @@
 package com.example.chatproject.bottomnav.newchat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,18 +9,58 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.chatproject.MainActivity;
 import com.example.chatproject.databinding.FragmentNewchatBinding;
+import com.example.chatproject.users.User;
+import com.example.chatproject.users.UserAdapter;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class NewChatFragment extends Fragment {
     private FragmentNewchatBinding binding;
-
+    private void loadUsers(){
+        ArrayList<User> users = new ArrayList<User>();
+        FirebaseDatabase.getInstance().getReference().child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot userSnapshot : snapshot.getChildren()){
+                    if (userSnapshot.getKey().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+                        continue;
+                    }
+                    String username = userSnapshot.child("email").getValue().toString();
+                    users.add(new User(username));
+                }
+                binding.usersRv.setLayoutManager(new LinearLayoutManager(getContext()));
+                binding.usersRv.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+                binding.usersRv.setAdapter(new UserAdapter(users));
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentNewchatBinding.inflate(inflater, container, false);
-
-
-
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getContext(), MainActivity.class));
+            }
+        });
+        loadUsers();
         return binding.getRoot();
     }
+
 }
